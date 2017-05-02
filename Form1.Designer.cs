0a99c26d8a18da5691e32f16c21bc4567a82c9dc
Sabrina Hemming <shemming444@gmail.com>
2017-05-02 18:34:02 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lab11_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startLabel = new System.Windows.Forms.Label();
+            this.startTextBox = new System.Windows.Forms.TextBox();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.endTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Location = new System.Drawing.Point(13, 37);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(29, 13);
+            this.startLabel.TabIndex = 0;
+            this.startLabel.Text = "Start";
+            // 
+            // startTextBox
+            // 
+            this.startTextBox.Location = new System.Drawing.Point(48, 34);
+            this.startTextBox.Name = "startTextBox";
+            this.startTextBox.Size = new System.Drawing.Size(159, 20);
+            this.startTextBox.TabIndex = 1;
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Location = new System.Drawing.Point(234, 37);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(26, 13);
+            this.endLabel.TabIndex = 2;
+            this.endLabel.Text = "End";
+            // 
+            // endTextBox
+            // 
+            this.endTextBox.Location = new System.Drawing.Point(266, 34);
+            this.endTextBox.Name = "endTextBox";
+            this.endTextBox.Size = new System.Drawing.Size(181, 20);
+            this.endTextBox.TabIndex = 3;
+            this.endTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.endTextBox_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(488, 248);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 334);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.endTextBox);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.startTextBox);
+            this.Controls.Add(this.startLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.TextBox startTextBox;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.TextBox endTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Lab11_NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int LINES = 80;
+
+        private double[] x = new double[LINES];
+        private double[] y = new double[LINES];
+
+        void graphPoints(double[] x, double[] y, Graphics g)
+        {
+            
+            // Clear the form space.
+            g.Clear(this.BackColor);
+
+            // Determine the size of the area in which we will draw.
+            // Size size = this.ClientSize;
+            Size size = pictureBox1.ClientSize;
+            
+            size.Height -= 5;
+
+            // Get the max and min x and y values.
+            double maxX = maxValue(x);
+            double maxY = maxValue(y);
+            double minX = minValue(x);
+            double minY = minValue(y);
+            double[] px = new double[x.Length];
+            double[] py = new double[y.Length];
+            Point[] points = new Point[x.Length];
+
+            // Scale the points to pixels.
+            for (int ip = 0; ip < x.Length; ip++)
+            {
+                px[ip] = (x[ip] - minX) / (maxX - minX) * size.Width;
+                py[ip] = size.Height - (y[ip] - minY) / (maxY - minY) * size.Height;
+                points[ip].X = (int) px[ip];
+                points[ip].Y = (int) py[ip];
+            }
+
+            Pen bluePen = new Pen(Color.Blue);
+            g.DrawCurve(bluePen, points);
+        }
+
+        double maxValue(double[] x)
+        {
+            double max = x[0];
+            foreach (double a in x)
+            {
+                if (max < a) max = a;
+            }
+            return max;
+        }
+
+        double minValue(double[] x)
+        {
+            double min = x[0];
+            foreach (double a in x)
+            {
+                if (min > a) min = a;
+            }
+            return min;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (startTextBox.Text.Length == 0 || endTextBox.Text.Length == 0)
+            {
+                return;
+            }
+            
+            // Get the current graphics object.
+            Graphics g = e.Graphics;
+
+            g.Clear(this.BackColor);
+
+            Size size = pictureBox1.ClientSize;
+
+            graphPoints(x, y, g);
+
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void endTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                if (startTextBox.Text.Length == 0 || endTextBox.Text.Length == 0)
+                {
+                    return;
+                }
+
+                double start, end;
+                if (!double.TryParse(startTextBox.Text.ToString(), out start))
+                {
+                    start = 0;
+                }
+
+                if (!double.TryParse(endTextBox.Text.ToString(), out end))
+                {
+                    end = start + 5;
+                }
+
+                if (end < start)
+                {
+                    return;
+                }
+
+                double tmp = start;
+                double span = end - start;
+                double increment = span / LINES;
+
+                for (int i = 0; i < LINES; i++)
+                {
+                    x[i] = tmp;
+                    y[i] = Math.Sin(tmp);
+                    tmp += increment;
+                }
+
+                pictureBox1.Invalidate();
+
+            }
+        }
+    }
+}
